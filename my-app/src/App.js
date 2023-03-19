a318ffbb966c3d2ae951f017b801e618fe07ba17
VivianaCodes <viviana.milone3@gmail.com>
2023-03-19 15:05:45 +0100

    Created App

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Search from "./Search";
+
+function App() {
+  return (
+    <div className="App">
+      <div class="weather-app">
+        <Search />
+        <Overview />
+      </div>
+    </div>
+  );
+}
+
+export default App;
